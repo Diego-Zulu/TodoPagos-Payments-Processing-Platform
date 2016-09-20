7d176ccc6e19886ceaa44296007e15c98767b7e1
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-09-20 17:35:36 -0300

    Implementación del test BeAbleToReturnItsDataTest() en la clase DateField. Estado: No compila.

--- TodoPagos/Tests/DateFieldShould.cs
+++ TodoPagos/Tests/DateFieldShould.cs
@@ -6,6 +6,16 @@ namespace Tests
     [TestClass]
     public class DateFieldShould
     {
+        [TestMethod]
+        public void BeAbleToReturnItsDataTest()
+        {
+            DateField dateField = new DateField();
 
+            DateTime date = DateTime.Today;
+            dateField.Data = date;
+            string expectedResult = date.ToShortDateString();
+
+            Assert.AreEqual(expectedResult, dateField.GetData());
+        }
     }
 }

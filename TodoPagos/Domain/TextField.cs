5dea5b2276304e6bbeb71b0285d137adba8656ae
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-09-20 16:48:02 -0300

    Implementación del test BeAbleToReturnItsDataTest() en la clase TextFieldShould. Estado: Error.

---
+++ TodoPagos/Domain/TextField.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TextField : IField
+    {
+        public string Data { get; set; }
+
+        public override IField FillAndClone(string dataToBeFilledWith)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TodoPagos/Tests/TextFieldShould.cs
+++ TodoPagos/Tests/TextFieldShould.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
 
 namespace Tests
 {
     [TestClass]
     public class TextFieldShould
     {
+        [TestMethod]
+        public void BeAbleToReturnItsDataTest()
+        {
+            TextField textField = new TextField();
+
+            string expectedResult = "";
+
+            Assert.AreEqual(expectedResult, textField.GetData());
+        }
+
         [TestMethod]
         public void AllowToBeFilledTest()
         {
@@ -14,7 +25,7 @@ namespace Tests
             IField newTextField = textField.FillAndClone("hola");
             string expectedResult = "hola";
 
-            Assert.AreEqual(expectedResult, newTextField.Data);
+            Assert.AreEqual(expectedResult, newTextField.GetData());
         }
     }
 }

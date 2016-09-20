c28d88e702b4d2ab7fa6c3e124a5766cde88d4dd
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-09-20 14:01:41 -0300

    Se creó la clase de Prueba "UserShould" en el paquete Tests. Dentro de ella, se implementó la prueba "NotHaveAnInvalidEmail()". Estado: No Compila

---
+++ TodoPagos/Tests/UserShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UserShould
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NotHaveAnInvalidEmail()
+        {
+            string email = "invalid";
+            string name = "Diego";
+
+            UserShould notValidUser = new UserShould(name, email);
+        }
+    }
+}

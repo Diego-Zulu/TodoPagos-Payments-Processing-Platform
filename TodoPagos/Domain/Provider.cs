4437de285e47272640399c2629748dbe16dd9c3b
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-09-21 17:15:33 -0300

    Implementación del test FailWhenChangingCommissionToANegativeValueTest() en la clase ProviderShould. Estado: OK.

--- TodoPagos/Domain/Provider.cs
+++ TodoPagos/Domain/Provider.cs
@@ -20,6 +20,7 @@ namespace Domain
 
         public void ChangeCommission(long newValue)
         {
+            if (newValue < 0) throw new ArgumentException();
             Commission = newValue;
         }
     }

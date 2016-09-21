a07bb72b01b6a94e025b9b9ed7354a106640610f
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-09-21 19:46:35 -0300

    Se crearon las clases "PayMethod", "CashPayMethod", "DebitPayMethod" y "CreditPayMethod"  en el paquete Domain. Se creó la clase "CashPayMethodShould" en el paquete Tests. Se implementó la prueba "BeAbleToPay()" en "CashPayMethod". Estado: No Compila

---
+++ TodoPagos/Tests/CashPayMethodShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace Tests
+{
+    [TestClass]
+    public class CashPayMethodShould
+    {
+        [TestMethod]
+        public void BeAbleToPay()
+        {
+            PayMethod payMethod = new CashPayMethod();
+            int paymentTotal = 1000;
+            int moneyPayedWith = 2500;
+
+            Assert.AreEqual(moneyPayedWith - paymentTotal, payMethod.Change);
+        }
+    }
+}

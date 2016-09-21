5e0aca942fed67d374a857f752deb360ba0ed019
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-09-21 17:00:08 -0300

    Implementación del test BeAbleToModifyHisCommissionTest() en la clase ProviderShould. Estado: OK y Refactoreado.

---
+++ TodoPagos/Domain/Provider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Provider
+    {
+        public long Commission { get; set; }
+
+        public string Name { get; set; }
+
+        public Provider(string aName, long aCommission)
+        {
+            Commission = aCommission;
+            Name = aName;
+        }
+
+        public void ChangeCommission(long newValue)
+        {
+            Commission = newValue;
+        }
+    }
+}

3135ad381cd87a8c5520c35c46aa4f9fc5501e87
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-09-21 14:47:52 -0300

    Se creó la clase de pruebas "RoleShould". Se implementó la prueba "NotCreateMultipleInstancesOfSameRole()" en la clase mencionada. Estado: No Compila

---
+++ TodoPagos/Tests/RoleShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace Tests
+{
+    [TestClass]
+    public class RoleShould
+    {
+        [TestMethod]
+        public void NotCreateMultipleInstancesOfSameRole()
+        {
+            Role firstCashierRole = CashierRole.GetInstance();
+            Role secondCashierRole = CashierRole.GetInstance();
+
+            Assert.AreSame(firstCashierRole, secondCashierRole);
+        }
+    }
+}

4745bd3bb3f5065f9d3ce1f02e4b177028ac8df4
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-09-22 16:23:08 -0300

    Se implementó la prueba "HavePositiveAmountPayed()" en "PaymentShould". Estado: No Compila

--- TodoPagos/Tests/PaymentShould.cs
+++ TodoPagos/Tests/PaymentShould.cs
@@ -14,5 +14,14 @@ namespace Tests
             PayMethod paymentMethod = null;
             Payment newPayment = new Payment(paymentMethod);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void HavePositiveAmountPayed()
+        {
+            int amountPayed = -1000;
+            PayMethod paymentMethod = new DebitPayMethod(DateTime.Now);
+            Payment newPayment = new Payment(paymentMethod, amountPayed);
+        }
     }
 }

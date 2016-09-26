479bca6c306fb79da90d4420af76f1f77aa1d514
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-09-26 19:23:19 -0300

    Implementación del test FailIfNameIsNullOrWhitespace() en la clase ProviderShould. Estado: OK.

--- TodoPagos/Domain/Provider.cs
+++ TodoPagos/Domain/Provider.cs
@@ -15,6 +15,7 @@ namespace Domain
 
         public Provider(string aName, double aCommission, ICollection<IField> fields)
         {
+            if (String.IsNullOrWhiteSpace(aName)) throw new ArgumentException();
             CheckForPossibleErrors(fields, aCommission);
             Commission = aCommission;
             Name = aName;

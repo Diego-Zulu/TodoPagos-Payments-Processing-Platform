975f29c24567ecd399885228aef396f1d5ab1421
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-10-05 18:52:20 -0300

    Se implemento "RecieveAProviderServiceOnCreation" en "ProvidersControllerShould". Estado: Error

--- TodoPagos/TodoPagos.Web.Api.Tests/ProvidersControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/ProvidersControllerShould.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using TodoPagos.Web.Api.Controllers;
+using TodoPagos.Web.Services;
 
 namespace TodoPagos.Web.Api.Tests
 {
---
+++ TodoPagos/TodoPagos.Web.Api/Controllers/ProvidersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TodoPagos.Web.Services;
+
+namespace TodoPagos.Web.Api.Controllers
+{
+    [RoutePrefix("api/v1/providers")]
+    public class ProvidersController : ApiController
+    {
+        public ProvidersController(IProviderService oneService)
+        {
+
+        }
+    }
+}

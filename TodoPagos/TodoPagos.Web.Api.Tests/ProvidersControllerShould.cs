46f8e085f214e12fc27a9b49e51d096907c89226
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-10-05 19:34:57 -0300

    Se implemento "BeAbleToReturnAllProvidersInRepository()" en "ProvidersControllerShould". Estado: No Compila

--- TodoPagos/TodoPagos.Web.Api.Tests/ProvidersControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/ProvidersControllerShould.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Moq;
 using TodoPagos.Web.Api.Controllers;
 using TodoPagos.Web.Services;
+using TodoPagos.Domain;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace TodoPagos.Web.Api.Tests
 {
@@ -28,6 +31,24 @@ namespace TodoPagos.Web.Api.Tests
             IProviderService nullProviderService = null;
 
             ProvidersController controller = new ProvidersController(nullProviderService);
+        }
+
+        [TestMethod]
+        public void BeAbleToReturnAllProvidersInRepository()
+        {
+            var allProviders = new[]
+            {
+                new Provider("Antel", 10, new List<IField>()),
+            new Provider("Devoto", 15, new List<IField>())
+        };
+            var mockProviderService = new Mock<IProviderService>();
+            mockProviderService.Setup(x => x.GetAllProviders()).Returns(allProviders);
+            ProvidersController controller = new ProvidersController(mockProviderService.Object);
+
+            IHttpActionResult actionResult = controller.GetProviders();
+            OkNegotiatedContentResult<IEnumerable<Provider>> contentResult = (OkNegotiatedContentResult<IEnumerable<Provider>>)actionResult;
+
+            Assert.AreSame(contentResult.Content, allProviders);
         }
     }
 }

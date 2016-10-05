db940c04d00a86d8312e3d4729ba0c8c3188b1e9
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-10-05 19:03:57 -0300

    Se implemento "RecieveAProviderServiceOnCreation" en "ProvidersControllerShould". Estado: Ok y Refactoreada

--- TodoPagos/TodoPagos.Web.Api/Controllers/ProvidersController.cs
+++ TodoPagos/TodoPagos.Web.Api/Controllers/ProvidersController.cs
@@ -11,9 +11,11 @@ namespace TodoPagos.Web.Api.Controllers
     [RoutePrefix("api/v1/providers")]
     public class ProvidersController : ApiController
     {
+        private readonly IProviderService providerService;
+
         public ProvidersController(IProviderService oneService)
         {
-
+            providerService = oneService;
         }
     }
 }

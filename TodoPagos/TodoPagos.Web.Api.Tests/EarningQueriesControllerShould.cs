86edf47bb09c2d91f5c3adc2a5cddfb4a65bbea9
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-10-07 18:46:23 -0300

    Implementación del test ReceiveAnEarningQueriesServiceOnCreation() en la clase EarningQueriesControllerShould. Estado: OK y Refactoreado.

--- TodoPagos/TodoPagos.Web.Api.Tests/EarningQueriesControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/EarningQueriesControllerShould.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TodoPagos.Web.Services;
+using TodoPagos.Web.Api.Controllers;
 
 namespace TodoPagos.Web.Api.Tests
 {
---
+++ TodoPagos/TodoPagos.Web.Api/Controllers/EarningQueriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TodoPagos.Web.Services;
+
+namespace TodoPagos.Web.Api.Controllers
+{
+
+    [RoutePrefix("api/v1/users")]
+    public class EarningQueriesController : ApiController
+    {
+        private readonly IEarningQueriesService earningQueriesService;
+
+        public EarningQueriesController(IEarningQueriesService service)
+        {
+            earningQueriesService = service;
+        }
+    }
+}

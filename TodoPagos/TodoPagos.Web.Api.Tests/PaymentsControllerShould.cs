40099aa915e390abcf1c7ce494a977ca5f161b0d
Bruno Ferrari <bferr42@gmail.com>
2016-10-07 11:47:41 -0300

    Implementación del test RecieveAPaymentServiceOnCreation() en la clase PaymentsControllerShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Web.Api.Tests/PaymentsControllerShould.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoPagos.Web.Api.Tests
+{
+    [TestClass]
+    public class PaymentsControllerShould
+    {
+        [TestMethod]
+        public void RecieveAPaymentServiceOnCreation()
+        {
+            var mockPaymentService = new Mock<IPaymentService>();
+
+            PaymentsController controller = new PaymentsController(mockPaymentService.Object);
+        }
+    }
+}

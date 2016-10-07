22bcc1a9af7a0e414a7645b2bf409a17cd0ba2a2
Bruno Ferrari <bferr42@gmail.com>
2016-10-07 12:08:10 -0300

    Implementación del test BeAbleToReturnAllPaymentsInRepository() en la clase PaymentsControllerShould. Estado: No compila.

--- TodoPagos/TodoPagos.Web.Api.Tests/PaymentsControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/PaymentsControllerShould.cs
@@ -3,6 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoPagos.Web.Api.Controllers;
 using Moq;
 using TodoPagos.Web.Services;
+using TodoPagos.Domain;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace TodoPagos.Web.Api.Tests
 {
@@ -24,6 +28,24 @@ namespace TodoPagos.Web.Api.Tests
             IPaymentService service = null;
 
             PaymentsController controller = new PaymentsController(service);
+        }
+
+        [TestMethod]
+        public void BeAbleToReturnAllPaymentsInRepository()
+        {
+            var allPayments = new[]
+            {
+                new Payment(new CashPayMethod(100, DateTime.Now), 100, new List<Receipt>()),
+            new Payment(new DebitPayMethod(DateTime.Now), 100, new List<Receipt>())
+            };
+            var mockPaymentService = new Mock<IPaymentService>();
+            mockPaymentService.Setup(x => x.GetAllPayments()).Returns(allPayments);
+            PaymentsController controller = new PaymentsController(mockPaymentService.Object);
+
+            IHttpActionResult actionResult = controller.GetPayments();
+            OkNegotiatedContentResult<IEnumerable<Payment>> contentResult = (OkNegotiatedContentResult<IEnumerable<Payment>>)actionResult;
+
+            Assert.AreSame(contentResult.Content, allPayments);
         }
     }
 }

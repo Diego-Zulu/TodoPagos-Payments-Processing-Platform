4d2bbbaf39240411f8a6382c8e4df8325e102ed6
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-10-07 17:24:53 -0300

    Implementación del test BeAbleToPostNewPaymentIntoRepository() en la clase PaymentsControllerShould. Estado: No compila.

--- TodoPagos/TodoPagos.Web.Api.Tests/PaymentsControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/PaymentsControllerShould.cs
@@ -109,5 +109,29 @@ namespace TodoPagos.Web.Api.Tests
             IHttpActionResult actionResult = controller.GetPayment(payment.ID + 1);
             Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
         }
+
+        [TestMethod]
+        public void BeAbleToPostNewPaymentIntoRepository()
+        {
+            List<IField> emptyFields = new List<IField>();
+            NumberField field = new NumberField("Monto");
+            emptyFields.Add(field);
+            IField filledField = field.FillAndClone("100");
+            List<IField> fullFields = new List<IField>();
+            fullFields.Add(filledField);
+            Provider provider = new Provider("Antel", 3, emptyFields);
+            Receipt receipt = new Receipt(provider, fullFields, 100);
+            List<Receipt> list = new List<Receipt>();
+            list.Add(receipt);
+            Payment payment = new Payment(new CashPayMethod(100, DateTime.Now), 100, list);
+            var mockPaymentService = new Mock<IPaymentService>();
+            mockPaymentService.Setup(x => x.CreatePayment(payment)).Returns(1);
+            PaymentsController controller = new PaymentsController(mockPaymentService.Object);
+
+            IHttpActionResult actionResult = controller.PostPayment(payment);
+            CreatedAtRouteNegotiatedContentResult<Payment> contentResult = (CreatedAtRouteNegotiatedContentResult<Payment>)actionResult;
+
+            Assert.AreSame(contentResult.Content, payment);
+        }
     }
 }

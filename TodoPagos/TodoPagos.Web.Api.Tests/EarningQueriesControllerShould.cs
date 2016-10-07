7c5eec4f4ae0b35926085ceb8906574a168894de
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-10-07 18:33:49 -0300

    Implementación del test ReceiveAnEarningQueriesServiceOnCreation() en la clase EarningQueriesControllerShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Web.Api.Tests/EarningQueriesControllerShould.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoPagos.Web.Api.Tests
+{
+    [TestClass]
+    public class EarningQueriesControllerShould
+    {
+        [TestMethod]
+        public void ReceiveAnEarningQueriesServiceOnCreation()
+        {
+            var mockEarningQueriesService = new Mock<IEarningQueriesService>();
+
+            EarningQueriesController controller = new EarningQueriesController(mockEarningQueriesService.Object);
+        }
+    }
+}

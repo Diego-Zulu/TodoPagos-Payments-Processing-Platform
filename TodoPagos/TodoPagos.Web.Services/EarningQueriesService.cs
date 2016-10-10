c6154ea9af91630b27a5a8145c93fdba4dd8dad3
Bruno Ferrari <bferr42@gmail.com>
2016-10-10 14:01:51 -0300

    Implementación del test ReceiveAUnitOfWorkOnCreation() en la clase EarningQueriesServiceShould. Estado: OK y Refactoreado.

---
+++ TodoPagos/TodoPagos.Web.Services/EarningQueriesService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoPagos.Domain;
+using TodoPagos.Domain.Repository;
+
+namespace TodoPagos.Web.Services
+{
+    public class EarningQueriesService : IEarningQueriesService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public EarningQueriesService(IUnitOfWork oneUnitOfWork)
+        {
+            unitOfWork = oneUnitOfWork;
+        }
+
+        public int GetAllEarnings(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<Provider, int> GetEarningsPerProvider(DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

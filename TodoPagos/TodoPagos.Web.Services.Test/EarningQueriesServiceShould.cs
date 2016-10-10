0ecc48adbbf7fd632ec339dc96342d20b4d04855
Bruno Ferrari <bferr42@gmail.com>
2016-10-10 13:58:33 -0300

    Implementación del test ReceiveAUnitOfWorkOnCreation() en la clase EarningQueriesServiceShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Web.Services.Test/EarningQueriesServiceShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoPagos.Domain.Repository;
+using Moq;
+
+namespace TodoPagos.Web.Services.Test
+{
+    [TestClass]
+    public class EarningQueriesServiceShould
+    {
+        [TestMethod]
+        public void ReceiveAUnitOfWorkOnCreation()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            EarningQueriesService service = new EarningQueriesService(mockUnitOfWork.Object);
+        }
+    }
+}

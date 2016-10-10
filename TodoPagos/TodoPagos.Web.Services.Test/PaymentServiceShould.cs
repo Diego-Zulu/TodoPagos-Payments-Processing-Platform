366fe8ae61bc01133e326ee990c68bd0750799d8
Bruno Ferrari <bferr42@gmail.com>
2016-10-10 12:40:54 -0300

    Implementación del test ReceiveAUnitOfWorkOnCreation() en la clase PaymentServiceShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Web.Services.Test/PaymentServiceShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TodoPagos.Domain.Repository;
+
+namespace TodoPagos.Web.Services.Test
+{
+    [TestClass]
+    public class PaymentServiceShould
+    {
+        [TestMethod]
+        public void RecieveAUnitOfWorkOnCreation()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            PaymentService service = new PaymentService(mockUnitOfWork.Object);
+        }
+    }
+}

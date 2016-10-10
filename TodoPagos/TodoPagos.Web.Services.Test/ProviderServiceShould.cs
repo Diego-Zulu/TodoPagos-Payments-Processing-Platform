ba5d9e1b141e47fe1aa4c876f7596de1db67016c
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-10-10 08:45:15 -0300

    Se implementó "RecieveAUnitOfWorkOnCreation()" y "FailIfUnitOfWorkOnCreationIsNull()". Estado: No Compila

---
+++ TodoPagos/TodoPagos.Web.Services.Test/ProviderServiceShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoPagos.Domain.Repository;
+using Moq;
+using System.Collections.Generic;
+using TodoPagos.UserAPI;
+
+namespace TodoPagos.Web.Services.Test
+{
+    [TestClass]
+    public class ProviderServiceShould
+    {
+        [TestMethod]
+        public void RecieveAUnitOfWorkOnCreation()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            ProviderService service = new ProviderService(mockUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailIfUnitOfWorkOnCreationIsNull()
+        {
+            IUnitOfWork mockUnitOfWork = null;
+
+            ProviderService service = new ProviderService(mockUnitOfWork);
+        }
+    }
+}

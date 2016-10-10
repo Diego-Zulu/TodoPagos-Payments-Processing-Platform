59f942ffc6417416fc78dbd1fb1a8e0f0181b8f4
Bruno Ferrari <bferr42@gmail.com>
2016-10-10 12:54:15 -0300

    Implementación del test BeAbleToGetAllPaymentsFromRepository() en la clase PaymentServiceShould. Estado: No compila.

--- TodoPagos/TodoPagos.Web.Services.Test/PaymentServiceShould.cs
+++ TodoPagos/TodoPagos.Web.Services.Test/PaymentServiceShould.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TodoPagos.Domain.Repository;
+using System.Collections.Generic;
+using TodoPagos.Domain;
 
 namespace TodoPagos.Web.Services.Test
 {
@@ -23,6 +25,18 @@ namespace TodoPagos.Web.Services.Test
             IUnitOfWork mockUnitOfWork = null;
 
             PaymentService service = new PaymentService(mockUnitOfWork);
+        }
+
+        [TestMethod]
+        public void BeAbleToGetAllPaymentsFromRepository()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(x => x.PaymentRepository.Get(null, null, ""));
+            PaymentService paymentService = new PaymentService(mockUnitOfWork.Object);
+
+            IEnumerable<Payment> payments = paymentService.GetAllPayments();
+
+            mockUnitOfWork.VerifyAll();
         }
     }
 }

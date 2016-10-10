2c2d1f97f453d4c631d982ca01731c9d5f404ec6
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-10-10 08:49:54 -0300

    Se implementó "RecieveAUnitOfWorkOnCreation()" y "FailIfUnitOfWorkOnCreationIsNull()". Estado: Ok y Refactoreadas

---
+++ TodoPagos/TodoPagos.Web.Services/ProviderService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoPagos.Domain;
+using TodoPagos.Domain.Repository;
+
+namespace TodoPagos.Web.Services
+{
+    public class ProviderService : IProviderService
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProviderService(IUnitOfWork oneUnitOfWork)
+        {
+            MakeSureTargetUnitOfWorkIsNotNull(oneUnitOfWork);
+            unitOfWork = oneUnitOfWork;
+        }
+
+        private void MakeSureTargetUnitOfWorkIsNotNull(IUnitOfWork oneUnitOfWork)
+        {
+            if (oneUnitOfWork == null)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public int CreateProvider(Provider targetProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProvider(int providerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Provider> GetAllProviders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Provider GetSingleProvider(int providerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProvider(int providerId, Provider targetProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

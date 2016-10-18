ed578a1259fe3290254729ac5de47306ed76bc20
Bruno Ferrari <ferrari.bruno@outlook.es>
2016-10-17 21:35:20 -0300

    Implementación del test FailWhenTheProvidedDateIsNotInGMTFormat() en la clase DebitPayShould. Estado: Error.

--- TodoPagos/TodoPagos.Domain.Tests/CashPayMethodShould.cs
+++ TodoPagos/TodoPagos.Domain.Tests/CashPayMethodShould.cs
@@ -57,10 +57,21 @@ namespace TodoPagos.Domain.Tests
         {
             int paymentTotal = 1000;
             int moneyPayedWith = 2500;
-            DateTime oneDate = DateTime.Parse("10/10/2010");
+            DateTime oneDate = DateTime.Parse("Sun, 25 Oct 2015 10:30:41 GMT");
             PayMethod payMethod = new CashPayMethod(oneDate);
 
             Assert.AreEqual(oneDate, payMethod.PayDate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailWhenTheProvidedDateIsNotInGMTFormat()
+        {
+            int paymentTotal = 1000;
+            int moneyPayedWith = 2500;
+            DateTime oneDate = DateTime.Parse("05/05/2010");
+
+            PayMethod payMethod = new CashPayMethod(oneDate);
         }
     }
 }

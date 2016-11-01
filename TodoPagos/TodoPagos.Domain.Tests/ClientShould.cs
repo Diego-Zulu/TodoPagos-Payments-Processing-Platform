70a296224eaa1f18e9561f8e916a13147be12468
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-01 16:08:42 -0300

    Se implementó "RecieveNameIDCardAndPhoneAtCreation()" en "ClientShould". Estado: Ok y Refactoreada

--- TodoPagos/TodoPagos.Domain.Tests/ClientShould.cs
+++ TodoPagos/TodoPagos.Domain.Tests/ClientShould.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace TodoPagos.Domain.Tests
 {
+    [TestClass]
     public class ClientShould
     {
+        [TestMethod]
+        public void RecieveNameIDCardAndPhoneAtCreation()
+        {
+            string name = "Diego Zuluaga";
+            int idCard = 49018830;
+            int phone = 26666666;
+
+            Client newClient = new Client(name, idCard, phone);
+        }
     }
 }
---
+++ TodoPagos/TodoPagos.Domain/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPagos.Domain
+{
+    public class Client
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        int IDCard { get; set; }
+        int PhoneNumber { get; set; }
+
+        protected Client() { }
+
+        public Client(string newName, int newIDCard, int newPhoneNumber)
+        {
+            Name = newName;
+            IDCard = newIDCard;
+            PhoneNumber = newPhoneNumber;
+        }
+    }
+}

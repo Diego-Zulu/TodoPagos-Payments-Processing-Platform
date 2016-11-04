87e326f44bab4cc78e4bb7a93b0d910db0cd4eeb
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-04 13:06:29 -0300

    Se implementó "ReceiveNameDescriptionAndNeededPointsOnCreation()" en "ProductShould". Estado: Refactoreada

--- TodoPagos/TodoPagos.Domain.Tests/ProductShould.cs
+++ TodoPagos/TodoPagos.Domain.Tests/ProductShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoPagos.Domain;
 
 namespace Tests
 {
---
+++ TodoPagos/TodoPagos.Domain/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoPagos.Domain
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NeededPoints { get; set; }
+
+        protected Product()
+        {
+
+        }
+
+        public Product(string newName, string newDescription, int newNeededPoints)
+        {
+            Name = newName;
+            Description = newDescription;
+            NeededPoints = newNeededPoints;
+        }
+    }
+}

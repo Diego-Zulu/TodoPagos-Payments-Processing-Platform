8b196db5aa18cd0922cf17dbcf688a83a9168842
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-04 13:02:36 -0300

    Se implementó "ReceiveNameDescriptionAndNeededPointsOnCreation()" en "ProductShould". Estado: No Compila

---
+++ TodoPagos/TodoPagos.Domain.Tests/ProductShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProductShould
+    {
+        [TestMethod]
+        public void ReceiveNameDescriptionAndNeededPointsOnCreation()
+        {
+            string name = "Manzana Roja";
+            string description = "Son mas frescas por la tarde";
+            int neededPoints = 10;
+
+            Product newProduct = new Product(name, description, neededPoints);
+        }
+    }
+}

ba2511f5dcd22206e0c1a93d6994626988bc8f6f
Bruno Ferrari <bferr42@gmail.com>
2016-11-04 13:07:32 -0300

    Implementación del test HaveAnActionType() en la clase LogEntryShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Domain.Tests/LogEntryShould.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class LogEntryShould
+    {
+        [TestMethod]
+        public void HaveAnActionType()
+        {
+            LogEntry newLogEntry = new LogEntry(ActionType.LOGIN);
+
+            Assert.IsNotNull(newLogEntry.ActionType);
+        }
+    }
+}

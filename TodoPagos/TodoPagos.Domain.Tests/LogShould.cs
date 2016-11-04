abeb496d8b1765199e4f30f9c542ba3a99b20dff
Bruno Ferrari <bferr42@gmail.com>
2016-11-04 14:26:30 -0300

    Implementación del test HaveAListOfAllLogEntries() en la clase LogShould. Estado: No compila.

---
+++ TodoPagos/TodoPagos.Domain.Tests/LogShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class LogShould
+    {
+        [TestMethod]
+        public void HaveAListOfAllLogEntries()
+        {
+            string userEmail = "bferr42@gmail.com";
+            Log newLog = new Log();
+            LogEntry newLogEntry = new LogEntry(ActionType.LOGIN, userEmail);
+            List<LogEntry> allEntries = new List<LogEntry>(){ newLogEntry};
+
+            newLog.AddEntry(newLogEntry);
+
+            Assert.AreSame(allEntries, newLog.Entries);
+        }
+    }
+}

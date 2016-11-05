83e25651da71fa026799343080505ec6d9c62e31
Bruno Ferrari <bferr42@gmail.com>
2016-11-05 19:32:35 -0300

    Implementación del test BeAbleToReturnAllLogEntriesBetweenTwoGivenDates() en la clase LogQueryFacadeShould. Estado: No compila.

--- TodoPagos/TodoPagos.AdminForm.Logic.Tests/LogQueryFacadeShould.cs
+++ TodoPagos/TodoPagos.AdminForm.Logic.Tests/LogQueryFacadeShould.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoPagos.Domain.Repository;
 using Moq;
+using Domain;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace TodoPagos.AdminForm.Logic.Tests
 {
@@ -15,6 +18,21 @@ namespace TodoPagos.AdminForm.Logic.Tests
             ILogStrategy strategy = null;
 
             LogQueryFacade facade = new LogQueryFacade(strategy);
+        }
+
+        [TestMethod]
+        public void BeAbleToReturnAllLogEntriesBetweenTwoGivenDates()
+        {
+            var mockStrategy = new Mock<ILogStrategy>();
+            LogQueryFacade facade = new LogQueryFacade(mockStrategy.Object);
+            LogEntry entry = new LogEntry(ActionType.LOGIN, "hola@hola.com");
+            ICollection<LogEntry> allEntries = new List<LogEntry>() { entry };
+            mockStrategy.Setup(u => u.GetEntries(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(allEntries);
+
+            ICollection<LogEntry> returnedEntries = facade.GetEntries(DateTime.MinValue, DateTime.MaxValue);
+
+            mockStrategy.VerifyAll();
+            CollectionAssert.AreEqual((ICollection)allEntries, (ICollection)returnedEntries);
         }
     }
 }

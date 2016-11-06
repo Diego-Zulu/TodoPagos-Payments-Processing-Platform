cc74b28cdf879bb247e71f6e6e7e26f5674f9e35
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-06 16:18:15 -0300

    Se implementó "FailWithNotFoundIfSingleUserIdDoesntExistInRepository()", "BeAbleToReturnSingleClientInRepository()" y "BeAbleToReturnAllClientsInRepository()" en "ClientsControllerShould". Estado: No Compila

--- TodoPagos/TodoPagos.Web.Api.Tests/ControllerUnitTests/ClientsControllerShould.cs
+++ TodoPagos/TodoPagos.Web.Api.Tests/ControllerUnitTests/ClientsControllerShould.cs
@@ -3,6 +3,11 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoPagos.Web.Services;
 using TodoPagos.Web.Api.Controllers;
 using Moq;
+using TodoPagos.Domain;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Linq;
 
 namespace TodoPagos.Web.Api.Tests.ControllerUnitTests
 {
@@ -31,6 +36,51 @@ namespace TodoPagos.Web.Api.Tests.ControllerUnitTests
             IClientService nullClientService = null;
 
             ClientsController controller = new ClientsController(nullClientService);
+        }
+
+        [TestMethod]
+        public void BeAbleToReturnAllClientsInRepository()
+        {
+            List<Client> allClients = new List<Client>
+            {
+                new Client("Manzana", "12345672", "26666666"),
+                new Client("Ruben Rada", "11111118", "26666666")
+            };
+            var mockClientService = new Mock<IClientService>();
+            mockClientService.Setup(x => x.GetAllClients(It.IsAny<string>())).Returns(allClients);
+            ClientsController controller = new ClientsController(mockClientService.Object);
+
+            IHttpActionResult actionResult = controller.GetClients();
+            OkNegotiatedContentResult<IEnumerable<Client>> contentResult = (OkNegotiatedContentResult<IEnumerable<Client>>)actionResult;
+
+            Assert.IsTrue(contentResult.Content.All(x => allClients.Contains(x)) &&
+                        allClients.All(x => contentResult.Content.Contains(x)));
+        }
+
+        [TestMethod]
+        public void BeAbleToReturnSingleClientInRepository()
+        {
+            Client singleClient = new Client("Ruben Rada", "11111118", "26666666");
+            var mockClientService = new Mock<IClientService>();
+            mockClientService.Setup(x => x.GetSingleClient(singleClient.ID, It.IsAny<string>())).Returns(singleClient);
+            ClientsController controller = new ClientsController(mockClientService.Object);
+
+            IHttpActionResult actionResult = controller.GetClient(singleClient.ID);
+            OkNegotiatedContentResult<Client> contentResult = (OkNegotiatedContentResult<Client>)actionResult;
+
+            Assert.AreEqual(contentResult.Content, singleClient);
+        }
+
+        [TestMethod]
+        public void FailWithNotFoundIfSingleUserIdDoesntExistInRepository()
+        {
+            Client singleClient = new Client("Ruben Rada", "11111118", "26666666");
+            var mockClientService = new Mock<IClientService>();
+            mockClientService.Setup(x => x.GetSingleClient(singleClient.ID, It.IsAny<string>())).Returns(singleClient);
+            ClientsController controller = new ClientsController(mockClientService.Object);
+
+            IHttpActionResult actionResult = controller.GetClient(singleClient.ID + 1);
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
         }
     }
 }
--- TodoPagos/TodoPagos.Web.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodoPagos.Web.Api.MvcApplication" Language="C#" %>

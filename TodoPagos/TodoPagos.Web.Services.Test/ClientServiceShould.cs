786e6f175339222a96c2da01e1d2a793eee2152a
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-06 18:16:56 -0300

    Se implementó "FailWithInvalidOperationExceptionIfToBeCreatedNewUserIsAlreadyInRepository()", "FailWithArgumentNullExceptionIfToBeCreatedNewClientIsNull()", "FailWithInvalidOperationExceptionIfToBeCreatedNewClientIsNotComplete()" y "BeAbleToCreateNewClientInRepository()" en "ClientServiceShould". Estado: Error

--- TodoPagos/TodoPagos.Web.Services.Test/ClientServiceShould.cs
+++ TodoPagos/TodoPagos.Web.Services.Test/ClientServiceShould.cs
@@ -75,5 +75,63 @@ namespace TodoPagos.Web.Services.Test
 
             Client returnedClient = clientService.GetSingleClient(5, It.IsAny<string>());
         }
+
+        [TestMethod]
+        public void BeAbleToCreateNewClientInRepository()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(un => un.CurrentSignedInUserHasRequiredPrivilege(It.IsAny<string>(), UserManagementPrivilege.GetInstance())).Returns(true);
+            mockUnitOfWork.Setup(un => un.ClientRepository.Insert(It.IsAny<Client>()));
+            mockUnitOfWork.Setup(un => un.Save());
+            ClientService clientService = new ClientService(mockUnitOfWork.Object);
+
+            Client singleClient = new Client("Ruben Rada", "11111111", "26666666");
+            int id = clientService.CreateClient(singleClient, It.IsAny<string>());
+
+            mockUnitOfWork.VerifyAll();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FailWithInvalidOperationExceptionIfToBeCreatedNewClientIsNotComplete()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(un => un.CurrentSignedInUserHasRequiredPrivilege(It.IsAny<string>(), UserManagementPrivilege.GetInstance())).Returns(true);
+            mockUnitOfWork.Setup(un => un.ClientRepository.Get(
+               It.IsAny<System.Linq.Expressions.Expression<Func<Client, bool>>>(), null, "")).Returns(new List<Client>());
+            ClientService clientService = new ClientService(mockUnitOfWork.Object);
+
+            Client singleClient = new Client("Ruben Rada", "11111111", "26666666");
+            singleClient.Name = "";
+            int id = clientService.CreateClient(singleClient, It.IsAny<string>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FailWithArgumentNullExceptionIfToBeCreatedNewClientIsNull()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(un => un.CurrentSignedInUserHasRequiredPrivilege(It.IsAny<string>(), UserManagementPrivilege.GetInstance())).Returns(true);
+            ClientService clientService = new ClientService(mockUnitOfWork.Object);
+
+            Client nullClient = null;
+            int id = clientService.CreateClient(nullClient, It.IsAny<string>());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FailWithInvalidOperationExceptionIfToBeCreatedNewUserIsAlreadyInRepository()
+        {
+            Client repeatedClient = new Client("Ruben Rada", "11111111", "26666666");
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(un => un.CurrentSignedInUserHasRequiredPrivilege(It.IsAny<string>(), UserManagementPrivilege.GetInstance())).Returns(true);
+            mockUnitOfWork
+                .Setup(un => un.ClientRepository.Get(
+                It.IsAny<System.Linq.Expressions.Expression<Func<Client, bool>>>(), null, ""))
+                .Returns(new[] { repeatedClient });
+
+            ClientService clientService = new ClientService(mockUnitOfWork.Object);
+            int id = clientService.CreateClient(repeatedClient, It.IsAny<string>());
+        }
     }
 }

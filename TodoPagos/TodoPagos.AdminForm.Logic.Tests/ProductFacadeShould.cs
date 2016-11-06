b73a2afa79e5bf9998be2a19ddec877b1c73b4df
Bruno Ferrari <bferr42@gmail.com>
2016-11-06 18:08:48 -0300

    Implementación del test BeAbleToAddANewProduct() en la clase ProductFacadeShould. Estado: OK y Refactoreado.

--- TodoPagos/TodoPagos.AdminForm.Logic.Tests/ProductFacadeShould.cs
+++ TodoPagos/TodoPagos.AdminForm.Logic.Tests/ProductFacadeShould.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TodoPagos.Domain.Repository;
+using Moq;
+using TodoPagos.ProductImporterLogic;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace TodoPagos.AdminForm.Logic.Tests
 {
@@ -13,6 +17,20 @@ namespace TodoPagos.AdminForm.Logic.Tests
         {
             IUnitOfWork mockUnitOfWork = null;
             ProductFacade facade = new ProductFacade(mockUnitOfWork);
+        }
+
+        [TestMethod]
+        public void BeAbleToAddANewProduct()
+        {
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            ProductFacade facade = new ProductFacade(mockUnitOfWork.Object);
+            mockUnitOfWork.Setup(u => u.ProductsRepository.Get(It.IsAny<Expression<Func<Product, bool>>>(), null, "")).Returns(new List<Product>());
+            mockUnitOfWork.Setup(u => u.ProductsRepository.Insert(It.IsAny<Product>()));
+            Product productToBeAdded = new Product("Cocina", "Una cocina", 200);
+
+            facade.AddProduct(productToBeAdded);
+
+            mockUnitOfWork.VerifyAll();
         }
     }
 }
--- TodoPagos/TodoPagos.AdminForm.Logic/ProductFacade.cs
+++ TodoPagos/TodoPagos.AdminForm.Logic/ProductFacade.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoPagos.Domain.Repository;
+using TodoPagos.ProductImporterLogic;
 
 namespace TodoPagos.AdminForm.Logic
 {
@@ -20,6 +21,33 @@ namespace TodoPagos.AdminForm.Logic
         private void CheckForNullUnitOfWork(IUnitOfWork aUnitOfWork)
         {
             if (aUnitOfWork == null) throw new ArgumentException();
+        }
+
+        public void AddProduct(Product productToBeAdded)
+        {
+            if (!AlreadyInRepository(productToBeAdded))
+            {
+                unitOfWork.ProductsRepository.Insert(productToBeAdded);
+                unitOfWork.Save();
+            }
+        }
+
+        private bool AlreadyInRepository(Product productToBeAdded)
+        {
+            IEnumerable<Product> allProducts = unitOfWork.ProductsRepository.Get(null, null, "");
+            foreach(Product product in allProducts)
+            {
+                if (product.Equals(productToBeAdded))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return unitOfWork.ProductsRepository.Get(null, null, "");
         }
 
     }

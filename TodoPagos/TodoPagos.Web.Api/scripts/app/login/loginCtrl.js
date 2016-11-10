56435448cc8e87a62de05dcd14cacd02c8e94d9f
Diego Zuluaga <diego_i_zuluaga@outlook.com>
2016-11-10 17:19:26 -0300

    Se implemento el sistema de inicio de sesion de angular

---
+++ TodoPagos/TodoPagos.Web.Api/scripts/app/login/loginCtrl.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    var todoPagosApp = angular.module('TodoPagos');
+
+    todoPagosApp.controller('Login.Controller', ["$scope", "$http", "$window", function ($scope, $http, $window) {
+
+        var ctrl = this;
+        
+        $scope.login = function () {
+            var info = { grant_type: 'password', username: ctrl.namelogin, password: ctrl.passlogin };
+            $http({
+                url: 'api/v1/login',
+                method: 'POST',
+                transformRequest: function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: info, 
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded' 
+                }
+            }).success(function (data, status, headers, config) {
+                $window.sessionStorage.token = data.access_token;
+                console.log(data.access_token);
+                  $('#alert_placeholder').html('<div class="alert alert-info"><a class="close" data-dismiss="alert">×</a><span>Bienvenido</span></div>')
+              })
+              .error(function (data, status, headers, config) {
+
+                  delete $window.sessionStorage.token;
+
+                  $('#alert_placeholder').html('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><span>Email o Pass inválida</span></div>')
+                  console.log(info);
+              });
+        };
+    }])
+})();
--- TodoPagos/TodoPagos.Web.Api/scripts/app/users/UsersCtrl.js
+++ TodoPagos/TodoPagos.Web.Api/scripts/app/users/UsersCtrl.js
@@ -3,14 +3,23 @@
 
     var todoPagosApp = angular.module('TodoPagos');
 
-    todoPagosApp.controller('Users.Controller', function ($http, $log) {
+    todoPagosApp.controller('Users.Controller', ["$http", "$scope", "$log", function ($http, $scope, $log) {
 
-        $http.get('http://localhost:4128')
-            .success(function(result){
+        var ctrl = this;
+
+        $scope.go = function () {
+
+            var path = window.location.hostname;
+
+            $http.get('/api/v1/users')
+            .success(function (result) {
                 ctrl.users = result;
+                $('#alert_placeholder').html();
             })
-            .error(function(data, status){
-                $log.error(data);
+            .error(function (data, status) {
+                $('#alert_placeholder').html('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><span>Error: No se pudo traer a los clientes. Código: '+status+'</span></div>')
             });
-    })
+        }
+        
+    }])
 })();
